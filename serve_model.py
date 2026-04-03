2c2dee722264d39e9b2f9b8197583db30626bb67
Ankit Rana <ankitsrana0457@gmail.com>
2026-04-03 22:27:25 +0530

    Update serve_model.py

--- serve_model.py
+++ serve_model.py
@@ -1,9 +1,14 @@
+#Import required libraries
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 import os
 
 app = Flask(__name__)
+
+@app.route('/')   #When someone visits the root URL (/), run the function below
+def home():       #This is the function that runs when the / route is accessed.
+    return "ML API is running 🚀"   #This sends a simple text response back to the browser or user. So when someone opens your API URL, they see: "ML API is running 🚀"
 
 # Load model safely
 model_path = os.path.join(os.path.dirname(__file__), 'iris_model.pkl')
